b5cbb2630fbaf7a98985cb4c3d4dcbe1e8b5e722
Arthur Sarmento <arthurbs17@gmail.com>
2022-01-11 16:15:49 -0300

    Class recipiente criada

--- classes/recipiente.py
+++ classes/recipiente.py
@@ -1,1 +1,30 @@
-# Desenvolva sua classe Recipiente aqui.
+class Recipiente:
+    def __init__(self, tamanho):
+        self.tamanho: float = tamanho if tamanho > 0 else 0
+        self.conteudo: float = 0
+        self.limpo: bool = True
+
+    def esvaziar(self) -> None:
+        self.conteudo = 0
+    
+    def esta_vazio(self) -> bool:
+        return True if self.conteudo == 0 else False
+    
+    def lavar(self) -> None:
+        self.conteudo = 0
+        self.limpo = True
+    
+    def esta_limpo(self) -> bool:
+        return self.limpo
+    
+    def estado(self) -> str:
+        return "limpo" if self.limpo else "sujo"
+    
+    def sujar(self) -> None:
+        self.limpo = False
+    
+    def __repr__(self) -> str:
+        return f"Um recipiente {self.estado()} não especificado"
+    
+    def __str__(self) -> str:
+        return f"Um recipiente {self.estado()} não especificado"
