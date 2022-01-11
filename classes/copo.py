5098aa8ed73c46a9b6afe44c46bc4e5fc91a66e7
Arthur Sarmento <arthurbs17@gmail.com>
2022-01-11 17:06:57 -0300

    Classes criadas, com respectivas funçoes

--- classes/copo.py
+++ classes/copo.py
@@ -1,1 +1,36 @@
-# Desenvolva sua classe Copo aqui.
+from classes import Recipiente
+from typing import Union
+
+class Copo(Recipiente):
+    def __init__(self, tamanho: float):
+        super().__init__(tamanho)
+
+    def encher(self, bebida: str = 'água') -> Union[str, None]:
+        if self.limpo:
+            self.conteudo = self.tamanho
+            self.bebida = bebida
+            self.sujar()
+        else:
+            return "Não se pode encher um copo sujo"
+    
+    def beber(self, quantidade: float) -> Union[str, None]:
+        if quantidade < 0:
+            return "Quantidade deve ser positiva"
+        if quantidade > self.conteudo:
+            return "Não há bebida suficiente no copo"
+        else:
+            self.conteudo -= quantidade
+    
+    def lavar(self) -> None:
+        super().lavar()
+        self.bebida = None
+
+    def __repr__(self) -> str:
+        if self.esta_vazio():
+            return f"Um copo vazio de {self.tamanho:.1f}ml"
+        return f"Um copo de {self.tamanho:.1f}ml contendo {self.conteudo:.1f}ml de {self.bebida}"
+
+    def __str__(self) -> str:
+        if self.esta_vazio():
+            return f"Um copo vazio de {self.tamanho:.1f}ml"
+        return f"Um copo de {self.tamanho:.1f}ml contendo {self.conteudo:.1f}ml de {self.bebida}"
--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+from classes import Recipiente, Copo
+
 if __name__ == "__main__":
     # Execute suas testagens manuais aqui
-    ...
+    rec = Recipiente(100)
+    print(rec)
+    print(rec.esta_limpo())
+    rec.sujar()
+    print(rec.esta_limpo())
+    rec.lavar()
+    print(rec.esta_limpo(),"\n")
+
+    cup = Copo(300)
+    print(cup)
+    cup.encher('cerveja')
+    print(cup)
+    cup.beber(180)
+    print(cup)
+    cup.lavar()
+    print(cup)
